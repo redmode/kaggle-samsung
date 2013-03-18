63b3ed3657b6d31b1d06928bdddcf1b27328f8db
redmode <gedranovich@gmail.com>
2013-03-18 19:35:52 +0300

    after miu, 18 mon

--- samsung.R
+++ samsung.R
@@ -9,8 +9,18 @@ set.seed(12345)
 # Loading reqired packages
 #
 require(caret)
-require(doMC)
-registerDoMC(2)
+#require(doMC)
+#registerDoMC(2)
+require(doSNOW)
+
+
+#
+# Utility function
+#
+mode <- function(x) {
+  ux <- unique(x)
+  ux[which.max(tabulate(match(x, ux)))]
+}
 
 #
 # Loading the data
@@ -18,7 +28,28 @@ registerDoMC(2)
 data.train <- read.csv("data/train.csv")
 data.test  <- read.csv("data/test.csv")
 
-# data.train <- subset(data.train, subject %in% c(1,3,6))
+#
+# Start cluster
+#
+#cl <- makeCluster(c("localhost","inform-3"), type="SOCK")
+#registerDoSNOW(cl)
+
+
+inform3 <-
+  list(host="192.168.130.169",
+       rscript="C:/Program Files/R/R-2.15.3/bin/Rscript.exe",
+       snowlib="C:/Program Files/R/R-2.15.3/library")
+
+nodes <- c(lapply(1:2, function(i) inform3), rep("localhost",2))
+
+cl <- makeCluster(nodes, type = "SOCK", manual=TRUE)
+registerDoSNOW(cl)
+
+
+#
+# Small subset
+#
+data.train <- subset(data.train, subject %in% c(1,3,6))
 
 #
 # Train data preparation
@@ -40,97 +71,67 @@ TX <- data.test[,1:561]
 #
 # Pre-processing and train controlling
 #
-pp <- preProcess(X, method=c("center","scale"))
-X <- predict(pp, X)
-cvCtrl <- trainControl(method='cv', number=10)
+#pp <- preProcess(X, method=c("center","scale"))
+pp <- preProcess(X, method="pca", thresh=0.90)
+X  <- predict(pp, X)
+TX <- predict(pp, TX)
+cvCtrl <- trainControl(method='cv', number=10, returnResamp="none", returnData=FALSE, verboseIter=TRUE)
 
 #
 # Training models on X[train] + Validating on X[!train] set
 #
 
-
-model3 <- train(X[train,], Y[train], method='parRF', trControl=cvCtrl)
-
-
-model4 <- train(X[train,], Y[train], method='mlpWeightDecay', trControl=myControl, trace=FALSE, preProcess=PP)
-model5 <- train(X[train,], Y[train], method='ppr', trControl=myControl, preProcess=PP)
-model6 <- train(X[train,], Y[train], method='earth', trControl=myControl, preProcess=PP)
-model7 <- train(X[train,], Y[train], method='glm', trControl=myControl, preProcess=PP)
-
-model9 <- train(X[train,], Y[train], method='gam', trControl=myControl, preProcess=PP)
-model10 <- train(X[train,], Y[train], method='glmnet', trControl=myControl, preProcess=PP)
-
 #
-gbmFit <- train(X[train,], Y[train], method='gbm', 
-                trControl=cvCtrl, preProcess=cvProc,
+parrfFit <- train(X[train,], Y[train], method='parRF', trControl=cvCtrl, tuneLength=5)
+#
+mlpFit <- train(X[train,], Y[train], method='mlpWeightDecay', trControl=cvCtrl, tuneLength=5, trace=FALSE)
+#
+glmnetFit <- train(X[train,], Y[train], method='glmnet', trControl=cvCtrl, tuneLength=5)
+#
+gbmFit <- train(X[train,], Y[train], method='gbm', trControl=cvCtrl,
                 tuneGrid=expand.grid(.interaction.depth=(2:5)*2, .n.trees=100, .shrinkage=0.1))
-confusionMatrix(predict(gdmFit$finalModel, newdata=X[!train,], type="response"), Y[!train])$overall[1]
 #
-svmFit <- train(X[train,], Y[train], method="svmRadial",
-                trControl=cvCtrl, preProcess=cvProc,
-                tuneLength=3, scaled=FALSE)
-confusionMatrix(predict(svmFit$finalModel, newdata=X[!train,], type="class"), Y[!train])$overall[1]
+svmFit <- train(X[train,], Y[train], method="svmRadial", trControl=cvCtrl,
+                tuneLength=5, scaled=FALSE)
 # 
 rfFit <- train(X[train,], Y[train], method="rf",
-               trControl=cvCtrl, tuneLength=3, scaled=FALSE)
-confusionMatrix(predict(rfFit$finalModel, newdata=X[!train,], type="response"), Y[!train])$overall[1]
+               trControl=cvCtrl, tuneLength=5, scaled=FALSE)
 #
-plsFit <- train(X[train,], Y[train], method="pls",
-                trControl=cvCtrl, preProcess=cvProc,
-                #tuneLength=ncol(X),
-                tuneGrid=expand.grid(.ncomp=290:310),
+plsFit <- train(X[train,], Y[train], method="pls", trControl=cvCtrl,
+                tuneLength=ncol(X),
+                #tuneGrid=expand.grid(.ncomp=290:310),
                 scaled=FALSE)
-confusionMatrix(predict(plsFit$finalModel, newdata=X[!train,], type="class"), Y[!train])$overall[1]
 #
-knnFit <- train(X[train,], Y[train], method="knn",
-                trControl=cvCtrl, preProcess=cvProc,
-                tuneLength=5)
-pr <- predict(knnFit$finalModel, newdata=X[!train,], type="class")
-confusionMatrix(pr, Y[!train])$overall[1]
+knnFit <- train(X[train,], Y[train], method="knn", trControl=cvCtrl, tuneLength=5)
 
+#
+# Stop cluster
+#
+stopCluster(cl)
 
 #
 # Combining models
 #
-models <- list(gbmFit, svmFit, rfFit, plsFit, knnFit)
+models <- list(gbmFit, svmFit, rfFit, parrfFit, plsFit, knnFit, mlpFit, glmnetFit)
 
+pred <- as.data.frame(predict(models, newdata=X[!train,], type="raw"))
+colnames(pred) <- lapply(1:length(models), function(i) models[[i]]$method)
+pred$vote <- sapply(1:nrow(pred), function(i) mode(pred[i,])[1,1])
 
-# pred <- predict(models, newdata=test[,-c(N)], type="raw")
-preds <- extractPrediction(models, testX=test[,-c(N)], testY=test[,c(N)])
-test.preds <- subset(preds, dataType=="Test")
+res <- sapply(1:ncol(pred), function(i) caret::confusionMatrix(pred[,i], Y[!train])$overall[1])
+names(res) <- colnames(pred)
+res
 
-# confusionMatrix(test.preds$pred, test.preds$obs)
+##
+## After prune
+##
+models <- models[-c(which(res<0.8))]
 
-M <- length(models)
-K <- (nrow(test.preds)/M)
-test.preds$id <- rep(1:K, M)
-# head(test.preds[,c(2,5,6)],10)
+pred <- as.data.frame(predict(models, newdata=X[!train,], type="raw"))
+colnames(pred) <- lapply(1:length(models), function(i) models[[i]]$method)
+pred$vote <- sapply(1:nrow(pred), function(i) mode(pred[i,])[1,1])
 
-rs <- reshape(test.preds[,c(2,5,6)], v.names="pred", idvar="id", timevar="object", direction="wide")
-# head(rs)
-# nrow(rs)
-
-mode <- function(x) {
-  ux <- unique(x)
-  ux[which.max(tabulate(match(x, ux)))]
-}
-
-vote.pred <- sapply(1:K, function(i) mode(rs[i,-c(1)])[1,1])
-# length(vote.pred)
-
-cm <- confusionMatrix(vote.pred, test$activity)
-cm$overall[1]
-
-require(xtable)
-xtable(cm$byClass)
-
-confusionMatrix(test.preds$pred[test.preds$object=="svm"], test$activity)$overall[1]
-confusionMatrix(test.preds$pred[test.preds$object=="rf"], test$activity)$overall[1]
-confusionMatrix(test.preds$pred[test.preds$object=="pls"], test$activity)$overall[1]
-confusionMatrix(test.preds$pred[test.preds$object=="knn"], test$activity)$overall[1]
-confusionMatrix(test.preds$pred[test.preds$object=="gbm"], test$activity)$overall[1]
-
-
-
-
+res <- sapply(1:ncol(pred), function(i) caret::confusionMatrix(pred[,i], Y[!train])$overall[1])
+names(res) <- colnames(pred)
+res
 
